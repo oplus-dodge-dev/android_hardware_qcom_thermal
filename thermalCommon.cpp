c192b67fd7eeed11d00a50281c4e0c4b15f4005f
Manaf Meethalavalappu Pallikunhi <quic_manafm@quicinc.com>
2023-08-30 14:28:30 +0530

    thermal-hal: Fix hysteresis calculation not accounted properly
    Thermal hal uses default hysteresis for all types of sensor
    irrespective of different scaling factor. Consider scaling
    factor of each sensor while calculating hysteresis.
    
    Change-Id: Iee7ae837c5bea16ca0a320fcdf3a2f67dad52bb6

--- thermalCommon.cpp
+++ thermalCommon.cpp
@@ -45,7 +45,7 @@ SPDX-License-Identifier: BSD-3-Clause-Clear */
 
 #define MAX_LENGTH		50
 #define MAX_PATH		(256)
-#define DEFAULT_HYSTERESIS	5000
+#define DEFAULT_HYSTERESIS	5
 #define THERMAL_SYSFS		"/sys/class/thermal/"
 #define TZ_DIR_NAME		"thermal_zone"
 #define TZ_DIR_FMT		"thermal_zone%d"
@@ -436,12 +436,12 @@ int ThermalCommon::estimateSeverity(struct therm_sensor& sensor)
 				!isnan(sensor.thresh.hotThrottlingThresholds[idx]) &&
 				temp >=
 				(sensor.thresh.hotThrottlingThresholds[idx] -
-				DEFAULT_HYSTERESIS / sensor.mulFactor)) ||
+				DEFAULT_HYSTERESIS)) ||
 				(!sensor.positiveThresh &&
 				!isnan(sensor.thresh.coldThrottlingThresholds[idx]) &&
 				temp <=
 				(sensor.thresh.coldThrottlingThresholds[idx] +
-				DEFAULT_HYSTERESIS / sensor.mulFactor)))
+				DEFAULT_HYSTERESIS)))
 				break;
 			continue;
 		}
@@ -559,9 +559,10 @@ void ThermalCommon::initThreshold(struct therm_sensor& sensor)
 				(int)sensor.t.throttlingStatus]
 					* sensor.mulFactor;
 		if (!isnan(next_trip))
-			hyst_temp = (next_trip - curr_trip) + DEFAULT_HYSTERESIS;
+			hyst_temp = (next_trip - curr_trip) + (DEFAULT_HYSTERESIS *
+					sensor.mulFactor);
 		else
-			hyst_temp = DEFAULT_HYSTERESIS;
+			hyst_temp = DEFAULT_HYSTERESIS * sensor.mulFactor;
 		LOG(DEBUG) << "Sensor: " << sensor.t.name << " hysteresis:"
 			<< hyst_temp << std::endl;
 		snprintf(file_name, sizeof(file_name), HYST_FILE_FORMAT,
